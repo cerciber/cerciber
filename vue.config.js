909948b70a36b6aed1e41baa6651699f9888c47c
Cesar Augusto Torres Ardila <ceatorresar@unal.edu.co>
2022-12-13 15:19:37 -0500

    correction

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/cerciber/'
+    ? '/'
     : '/',
   runtimeCompiler: true
 })
