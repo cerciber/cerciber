90ecd0c17188d306a6925fcdf74963e85e8b4726
Cesar Augusto Torres Ardila <ceatorresar@unal.edu.co>
2022-12-13 14:06:40 -0500

    name corrections

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Cerciber/Cerciber.git master:gh-pages
+git push -f git@github.com:cerciber/cerciber.git master:gh-pages
 
 cd -
