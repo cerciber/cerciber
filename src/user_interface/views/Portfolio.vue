101da579ec575216bcf9b7a1b83cb2ec21ea93f0
Cesar Torres (Cerciber) <ceatorresar@unal.edu.co>
2023-01-31 16:48:06 -0500

    olds git projects

--- src/user_interface/views/Portfolio.vue
+++ src/user_interface/views/Portfolio.vue
@@ -52,6 +52,90 @@
       <div class="portfolio-shape-back-curve">
       </div>
     </div>
+    <div class="portfolio">
+      <div class="portfolio-part2">
+        <section class="portfolio-part2-section2">
+          <article>
+            <h3><b>Trimcode</b></h3>
+            <br>
+            <p>🔢 A <b>desktop application</b> that allows you to save and execute scripts from different programming languages, in such a way that it allows developers to create custom functionalities in the operating system and be able to use them at any time without the need to depend on other development tools. It allows you to run C, C++, Python, Java, and Javascript code. 🤼.</p>
+            <br>
+            <p>📝 I fully planted and developed the prototype using <b>Java SE</b>, directly accessing the base complicators of the programming languages, generalizing them through interfaces and using an <b>MVC</b> architecture 🌐.</p>
+          </article>
+        </section>
+      </div>
+      <div class="portfolio-part1">
+        <PhotoShapeRelative pathImage = "trimcode.png" :disableBack="true" title="TrimCode"></PhotoShapeRelative>
+      </div>
+    </div>
+    <div class="portfolio">
+      <div class="portfolio-part1">
+        <PhotoShapeRelative pathImage = "structfile.png" :disableBack="true" title="StructFile"></PhotoShapeRelative>
+      </div>
+      <div class="portfolio-part2">
+        <section class="portfolio-part2-section1">
+          <article>
+            <h3><b>Structfile</b></h3>
+            <br>
+            <p>🔢 It is a <b>programming language</b> of mine whose purpose is to facilitate the treatment of plain text, files and directories in the operating system. It consists of facilities to access the file system, handles treatment of variables, conditional and repetitive structures, as well as its own syntax to perform basic operations on data groups 🤼.</p>
+            <br>
+            <p>📝 I created the language execution environment in <b>Java SE</b>, and through <b>ANTLR</b> I established the specific grammar using <b>Python</b> and <b>RegEx</b> to develop the internal logic 🌐.</p>
+          </article>
+        </section>
+      </div>
+      <div class="portfolio-shape-back-curve">
+      </div>
+    </div>
+    <div class="portfolio">
+      <div class="portfolio-part2">
+        <section class="portfolio-part2-section2">
+          <article>
+            <h3><b>Findcode</b></h3>
+            <br>
+            <p>🔢 It is a <b>desktop application</b> that allows developers to save, manage and comment on their pieces of code interactively. It contains a search to find community codes and allows commenting on any code snippet that is underlined to explain the code without altering it 🤼.</p>
+            <br>
+            <p>📝 I developed this app hand in hand with someone else using <b>Java SE</b> for interfaces and interactions and <b>MySQL</b> for all data persistence 🌐.</p>
+          </article>
+        </section>
+      </div>
+      <div class="portfolio-part1">
+        <PhotoShapeRelative pathImage = "findcode.png" :disableBack="true" title="FindCode"></PhotoShapeRelative>
+      </div>
+    </div>
+    <div class="portfolio">
+      <div class="portfolio-part1">
+        <PhotoShapeRelative pathImage = "tankattack.png" :disableBack="true" title="TankAttack"></PhotoShapeRelative>
+      </div>
+      <div class="portfolio-part2">
+        <section class="portfolio-part2-section1">
+          <article>
+            <h3><b>Tankattack</b></h3>
+            <br>
+            <p>🔢 TankAttack is a <b>multiplayer desktop game</b> that combines the classic "capture the flag" with the excitement of gun war games. It consists of two tanks, each with its own base, whose objective is to obtain the flag of the enemy base by going through a terrain full of walls, stones, holes and water with the help of a cannon. Players will be forced to face each other in a cannon duel to protect their respective flags 🤼.</p>
+            <br>
+            <p>📝 I created this game with the help of another person using <b>Java SE</b> and <b>Java Swing</b> 🌐.</p>
+          </article>
+        </section>
+      </div>
+      <div class="portfolio-shape-back-curve">
+      </div>
+    </div>
+    <div class="portfolio">
+      <div class="portfolio-part2">
+        <section class="portfolio-part2-section2">
+          <article>
+            <h3><b>Quicklist</b></h3>
+            <br>
+            <p>🔢 QuickList is an <b>information system</b> for the integration of administrative-academic responsibilities developed for SENA. It is focused on the management of failures, activities, qualifications, study plans and export of predefined class stage formats. It was thought of as a viable solution to the shortcomings identified in SENA when it comes to monitoring students in terms of time and accessibility. 🤼.</p>
+            <br>
+            <p>📝 I led this project and developed this system in a team of 3 people using <b>Java SE</b> for create all system and <b>SQL Server</b> to do all the data persistence 🌐.</p>
+          </article>
+        </section>
+      </div>
+      <div class="portfolio-part1">
+        <PhotoShapeRelative pathImage = "quicklist.jpg" :disableBack="true" title="QuickList"></PhotoShapeRelative>
+      </div>
+    </div>
   </div>
 </template>
 
